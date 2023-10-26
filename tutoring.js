28d27367217198088d5feb6309d6d255cdd1a315
shriharmonic <132262703+shriharmonic@users.noreply.github.com>
2023-10-26 16:45:53 +1300

    Add files via upload

---
+++ tutoring.js
@@ -0,0 +1,15 @@
+console.log("Script is being executed");
+const navbar = `
+<nav>
+    <ul>
+      <li><a href="#who-we-are">Who We Are</a></li>
+      <li><a href="#pricing">Pricing</a></li>
+      <li><a href="#tutoring-options">Tutoring Options</a></li>
+      <li><a href="#grades">Average Grades</a></li>
+      <li><a href="#booking">Booking</a></li>
+      <li><a href="#feedback">Feedback</a></li>
+    </ul>
+  </nav>
+`;
+var footer = "<div>Website created by Shrihan. For use for tutoring site.</div>";
+let test = "<p>hi</p>";
